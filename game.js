0f5db1f46363b92953bc4d824fa08edfb6fa013f
meena-kaliswamy <77135509+meena-kaliswamy@users.noreply.github.com>
2021-03-12 23:08:43 -0800

    Changed to "onkeyup" for mobile touch

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ var level = 0;
 
 // Begin Game - If game has not started (level=0), document awaits a keyboard press. Next color in sequence is shown and game begins.
 
-$(document).on("keyup", function() {
+$(document).on("onkeyup", function() {
   if (level === 0) {
     nextInSequence();
   }
